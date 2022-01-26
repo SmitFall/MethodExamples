d03f2a3116b367daa1c1d7801f8b97c086c02997
Smitfall <captainfalconrsmith@gmail.com>
2022-01-26 08:54:52 -0700

    Add project files.

---
+++ MethodExamples/MethodExamples.vb
@@ -0,0 +1,35 @@
+﻿Option Explicit On
+
+Module MethodExamples
+
+    Sub Main()
+        Dim result As Integer
+
+        'Call Sub
+        AddTwoNumbers()
+        Console.WriteLine("In main")
+        Console.WriteLine(result)
+        Sumof(4, 6)
+
+        SayAMessage()
+
+        Console.ReadLine()
+    End Sub
+
+    Sub AddTwoNumbers()
+        Dim result As Integer
+        result = 2 + 2
+        Console.WriteLine("In sub")
+        Console.WriteLine(result)
+    End Sub
+
+    Sub Sumof(firstnumber As Integer, secondnumber As Integer)
+
+    End Sub
+
+    Sub SayAMessage()
+        Console.WriteLine("Say what again")
+
+    End Sub
+
+End Module
