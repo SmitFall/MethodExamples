2e432385c8b4cb0704c79c8ba43206a6f8a6bb4a
Smitfall <captainfalconrsmith@gmail.com>
2022-02-07 08:55:41 -0700

    looked at stuff

--- MethodExamples/MethodExamples.vb
+++ MethodExamples/MethodExamples.vb
@@ -1,17 +1,31 @@
 ﻿Option Explicit On
-
+Option Strict On
 Module MethodExamples
 
     Sub Main()
-        Dim result As Integer
+        Dim value As Integer
 
         'Call Sub
-        AddTwoNumbers()
-        Console.WriteLine("In main")
-        Console.WriteLine(result)
-        Sumof(4, 6)
+        'AddTwoNumbers()
+        'Console.WriteLine("In main")
+        'Console.WriteLine(result)
+        'Sumof(4, 6)
+        'value = SumResult(7, 7)
+        'value = value + 6
+        'Console.WriteLine(value)
 
-        SayAMessage()
+        'SayAMessage()
+        ' For i = 0 To 10
+
+        ' RandomInteger()
+        'Next
+        ' For i = 0 To 10
+        'Console.WriteLine(RandomNumber())
+        'Next
+
+        For i = 0 To 10
+            Console.WriteLine(Runningtotal(5))
+        Next
 
         Console.ReadLine()
     End Sub
@@ -23,13 +37,43 @@ Module MethodExamples
         Console.WriteLine(result)
     End Sub
 
-    Sub Sumof(firstnumber As Integer, secondnumber As Integer)
-
+    Sub Sumof(ByVal firstnumber As Integer, ByVal secondnumber As Integer)
+        Console.WriteLine(firstnumber + secondnumber)
     End Sub
+
+    Function SumResult(firstnumber As Integer, secondnumber As Integer) As Integer
+        Dim result As Integer
+        result = firstnumber + secondnumber
+        Return result
+    End Function
 
     Sub SayAMessage()
         Console.WriteLine("Say what again")
 
     End Sub
 
+    Sub RandomInteger()
+        Dim theRandomNumber As Integer
+
+        Randomize()
+
+        theRandomNumber = CInt(Rnd() * 10)
+
+        Console.WriteLine(theRandomNumber)
+
+
+    End Sub
+
+    Function RandomNumber() As Integer
+        Randomize()
+        Return CInt(Rnd() * 15)
+
+    End Function
+    Function Runningtotal(value As Integer) As Integer
+        Static total As Integer
+        total += value
+        Return Total
+
+
+    End Function
 End Module
